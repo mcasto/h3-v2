1a28942450442d7e206323683e0f13cdcd1507fa
Mike Casto <castoware@gmail.com>
2023-10-02 08:20:35 -0500

    Update

--- api/methods/get-items.php
+++ api/methods/get-items.php
@@ -5,7 +5,8 @@ function getItems($db, $request, $util)
     $items = $db->fetchAll("SELECT * FROM %n ORDER BY %by", $request->params->table, $request->params->sortBy, $request->params->sortDir);
 
     if ($request->params->table == 'albums') {
-      error_log(print_r($items, true));
+      $cols = $db->fetchAll("SHOW COLUMNS IN %n", $request->params->table);
+      error_log(print_r($cols, true));
     }
 
     $items = array_map(function ($item) {
