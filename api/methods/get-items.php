4fc1be4807b8f811ab6dc7fee3a8963e1fb1beb3
Mike Casto <castoware@gmail.com>
2023-10-02 08:18:10 -0500

    Update

--- api/methods/get-items.php
+++ api/methods/get-items.php
@@ -4,6 +4,10 @@ function getItems($db, $request, $util)
   try {
     $items = $db->fetchAll("SELECT * FROM %n ORDER BY %by", $request->params->table, $request->params->sortBy, $request->params->sortDir);
 
+    if ($request->params->table == 'albums') {
+      error_log(print_r($items, true));
+    }
+
     $items = array_map(function ($item) {
       if (isset($item->file_location)) {
         $item->mime = mime_content_type($_SERVER['DOCUMENT_ROOT'] . $item->file_location);
---
+++ assets/AdminPage.4adf039a.js
@@ -0,0 +1,1 @@
+import{_ as o,K as t,M as a,y as n,E as r,L as s}from"./index.1339cfb4.js";const c={name:"AdminPage",computed:{...t(s,["navigation"])}};function p(_,i,m,u,d,f){const e=a("router-view");return n(),r(e)}var l=o(c,[["render",p]]);export{l as default};
