2a7366b11736431f508c08cc28c4c0931bc6b9fb
Mike Casto <castoware@gmail.com>
2023-10-02 08:11:07 -0500

    Update

---
+++ assets/QCardSection.e98a0c8c.js
@@ -0,0 +1,1 @@
+import{c as e,a as r,b as n,k as s}from"./index.f8d1693d.js";var i=e({name:"QCardSection",props:{tag:{type:String,default:"div"},horizontal:Boolean},setup(a,{slots:t}){const o=r(()=>`q-card__section q-card__section--${a.horizontal===!0?"horiz row no-wrap":"vert"}`);return()=>n(a.tag,{class:o.value},s(t.default))}});export{i as Q};
---
+++ assets/QTr.e8f97ad9.js
@@ -0,0 +1,1 @@
+import{c as t,a,b as s,k as n}from"./index.f8d1693d.js";var c=t({name:"QTr",props:{props:Object,noHover:Boolean},setup(r,{slots:e}){const o=a(()=>"q-tr"+(r.props===void 0||r.props.header===!0?"":" "+r.props.__trClass)+(r.noHover===!0?" q-tr--no-hover":""));return()=>s("tr",{class:o.value},n(e.default))}});export{c as Q};
